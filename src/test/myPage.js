b5f0339d6ce00b1773a11e143f26dfe93b178856
HamzaAkbar067 <hakbar067@gmail.com>
2024-04-06 15:00:10 +0200

    remove comments

--- src/test/myPage.js
+++ src/test/myPage.js
@@ -7,7 +7,7 @@ function MyPage() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Add your App here.
         </p>
         <a
           className="App-link"
